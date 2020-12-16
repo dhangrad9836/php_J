b9c722b5ff4174b59d585222f633370108ddc0eb
Darren <darren.dhanpat@yahoo.com>
2020-12-16 07:38:24 -0500

    adding more php files

---
+++ doWhile.php
@@ -0,0 +1,23 @@
+<?php
+
+$cars = ["beemer", "benz", "bentley"];
+$i = 0;
+do {
+    echo "<h4>".$cars[$i]."</h4>";
+    $i++;
+}   while($i < count($cars));
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ whileLoop.php
@@ -0,0 +1,22 @@
+<?php
+
+$i = 0;
+while($i < 10){
+    echo "<h4>".$i."</h4>";
+    $i++;
+}
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
