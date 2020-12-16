6418dbf64235a09bbaf8148ebb7b7ef5070e8462
Darren <darren.dhanpat@yahoo.com>
2020-12-15 23:23:12 -0500

    adding for loop

---
+++ forLoop.php
@@ -0,0 +1,25 @@
+<?php
+
+for ($i = 0; $i <11; $i++){
+    echo "<h4>".$i."</h4>";
+}
+
+for ($i = 10; $i > 0; $i--){
+    echo "<h4>".$i."</h4>";
+}
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
